1e5256c3e46af25dd77e074eafa8db865e26b853
Branden Webb <brandenswebb@hotmail.com>
2021-03-19 14:37:57 -0400

    changes for tomcat

--- src/main/java/wcci/postit/PostitApplication.java
+++ src/main/java/wcci/postit/PostitApplication.java
@@ -2,12 +2,19 @@ package wcci.postit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PostitApplication {
+public class PostitApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PostitApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PostitApplication.class);
     }
 
 }
